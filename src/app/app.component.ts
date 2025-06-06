0e08498527831a1f184b8a2a6d7aa7daac7044b7
Marzi Ghasedian <marzighased@gmail.com>
2025-06-06 23:08:12 +0200

    add to cart function added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-shop';
+  products = [
+    {
+      id: 1,
+      name:'Laptop',
+      price: 1200,
+      description: 'powerful Laptop for   w ork and Play',
+      image: 'https://via.placeholder.com/200x150',
+      inStock: true
+    },
+    {
+      id: 2,
+      name: 'Smartphone',
+      price: 800,
+      description: 'samrtphone with advanced features',
+      image: 'https://via.placeholder.com/200x150',
+      inStock: true
+    },
+    {
+      id: 3,
+      name: 'Headphone',
+      price: 150,
+      description: 'Wireless Headphone with Excellent sound Quality',
+      image: 'https://via.placeholder.com/200x150',
+      inStock: false
+    }
+  ];
 }
---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss'
+})
+export class ProductCardComponent {
+  @Input() product: any;
+
+  addToCart() {
+    alert(`${this.product.name} added to Cart!`);
+  }
+}
